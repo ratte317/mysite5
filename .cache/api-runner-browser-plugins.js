d0ade2fa96450d06b1899647ce59b30f80a67447
ratte317 <37272121+ratte317@users.noreply.github.com>
2021-02-08 23:35:48 +0900

    test12

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"ESSENTIALS エッセンシャルズ","short_name":"ESSENTIALS","start_url":"/","background_color":"#ffffff","theme_color":"#477294","display":"standalone","icon":"src/images/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"7c75ee3d9b9d41ffca39847642924aa0"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
